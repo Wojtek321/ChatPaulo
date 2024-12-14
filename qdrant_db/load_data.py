dbe16adb7e81331e74df98b8bddb8ec5ac54478b
Wojciech Domański <wojtekdxd@gmail.com>
2024-12-14 20:56:19 +0100

    Refactor data loading script

--- qdrant_db/load_data.py
+++ qdrant_db/load_data.py
@@ -1,5 +1,6 @@
 from qdrant_client import QdrantClient, models
-from openai import OpenAI
+from langchain_qdrant import QdrantVectorStore
+from langchain_openai import OpenAIEmbeddings
 from langchain_core.documents import Document
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from dotenv import load_dotenv
@@ -11,87 +12,67 @@ import uuid
 load_dotenv()
 
 
-qdrantClient = QdrantClient("http://qdrant")
-openaiClient = OpenAI()
-
 DATA_FOLDER_PATH = pathlib.Path(__file__).parent / 'data'
+VECTOR_SIZE = 1536
+DISTANCE = models.Distance.DOT
+
+qdrant_client = QdrantClient("http://qdrant:6333")
+embeddings = OpenAIEmbeddings(model='text-embedding-3-small')
 
 
-def get_documents(file_path: pathlib.Path, metadata: dict) -> List[Document]:
-    """Loads document from a file and splits it into smaller chunks"""
-    with open(file_path, 'r') as f:
-        content = f.read().replace('\n', ' ')
+def create_collection(client: QdrantClient, collection_name) -> QdrantVectorStore:
+    """Creates new collection in Qdrant"""
+    if client.collection_exists(collection_name):
+        client.delete_collection(collection_name)
 
-    document = Document(
-        page_content=content,
-        metadata=metadata,
+    client.create_collection(
+        collection_name=collection_name,
+        vectors_config=models.VectorParams(size=VECTOR_SIZE, distance=DISTANCE)
     )
 
-    splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=50)
-    return splitter.split_documents([document])
+    return QdrantVectorStore(
+        client=client,
+        collection_name=collection_name,
+        embedding=embeddings,
+        distance=DISTANCE
+    )
 
 
-def generate_points(docs: List[Document], metadata_key: str) -> List[models.PointStruct]:
-    """Generates embeddings and PointStruct objects from a list of documents"""
-    points = []
+def get_documents(folder_path: pathlib.Path, metadata_key: str, chunk_size: int, chunk_overlap: int) -> List[Document]:
+    """Loads document from a file and splits it into smaller chunks"""
+    docs = []
 
-    for doc in docs:
-        response = openaiClient.embeddings.create(
-            model='text-embedding-3-small',
-            input=doc.page_content
-        )
-        embed = response.data[0].embedding
-
-        point = models.PointStruct(
-            id=str(uuid.uuid4()),
-            vector=embed,
-            payload={
-                'metadata': {'type': doc.metadata.get(metadata_key)},
-                'page_content': doc.page_content
-            }
-        )
-
-        points.append(point)
-
-    return points
-
-
-def process_folder_and_uplaod(folder_name: str, collection_name: str, metadata_key: str) -> None:
-    """Processes all the files in the folder and send them to Qdrant within the specified collection"""
-    folder_path = DATA_FOLDER_PATH / folder_name
-    files = os.listdir(folder_path)
-    points = []
-
-    for file in files:
+    for file in os.listdir(folder_path):
         file_path = folder_path / file
         metadata = {metadata_key: file.split('.')[0].lower()}
-        docs = get_documents(file_path, metadata)
-        pts = generate_points(docs, metadata_key)
-        points.extend(pts)
 
-    if not qdrantClient.collection_exists(collection_name):
-        qdrantClient.create_collection(
-            collection_name=collection_name,
-            vectors_config=models.VectorParams(size=1536, distance=models.Distance.DOT)
+        with open(file_path, 'r') as f:
+            content = f.read().replace('\n', ' ')
+
+        document = Document(
+            page_content=content,
+            metadata=metadata,
         )
 
-    qdrantClient.upload_points(
-        collection_name=collection_name,
-        points=points,
-        wait=True
-    )
-    print(f"Uploaded {len(points)} points to collection '{collection_name}'.")
+        docs.append(document)
+
+    splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    return splitter.split_documents(docs)
 
 
-if __name__ == "__main__":
-    process_folder_and_uplaod(
-        folder_name='pizzas',
-        collection_name='pizzas',
-        metadata_key='pizza_type'
-    )
+if __name__ == '__main__':
+    collection_name = 'pizzas'
+    vector_store = create_collection(qdrant_client, collection_name)
+    folder_path = DATA_FOLDER_PATH / 'pizzas'
+    docs = get_documents(folder_path, metadata_key='pizza_type', chunk_size=300, chunk_overlap=50)
+    ids = [str(uuid.uuid4()) for _ in range(len(docs))]
+    vector_store.add_documents(docs, ids=ids)
+    print(f"Uploaded {len(docs)} points to collection '{collection_name}'.")
 
-    process_folder_and_uplaod(
-        folder_name='FAQs',
-        collection_name='pizzeria',
-        metadata_key='file'
-    )
+    collection_name = 'pizzeria'
+    vector_store = create_collection(qdrant_client, collection_name)
+    folder_path = DATA_FOLDER_PATH / 'FAQs'
+    docs = get_documents(folder_path, metadata_key='file', chunk_size=200, chunk_overlap=30)
+    ids = [str(uuid.uuid4()) for _ in range(len(docs))]
+    vector_store.add_documents(docs, ids=ids)
+    print(f"Uploaded {len(docs)} points to collection '{collection_name}'.")
